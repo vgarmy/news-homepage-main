fba5553453e45def7e89788717ae75a5ff0b3c6a
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2025-02-24 12:25:15 +0100

    gui desktop

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import logo from './assets/logo.svg'
 function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen" role="main">
-      <div className='w-[86rem] px-[10rem] pb-[8rem] pt-[5rem] flex gap-[3rem] flex-col'>
+      <div className='w-[90rem] px-[10rem] pb-[8rem] pt-[5rem] flex gap-[3rem] flex-col'>
         <nav class="flex justify-between items-center">
           <div>
             <img src={logo} alt="Logo" class="h-10" />
@@ -22,14 +22,14 @@ function App() {
         {/* Grid Layout */}
         <div className="grid grid-cols-3 gap-6">
           {/* First row */}
-          <div className="col-span-2">
+          <div className="col-span-2 mb-8">
             <img src={data.main_article.image} alt={data.main_article.title} className="w-full h-auto mb-4" />
             <div className='grid grid-cols-2 gap-6'>
               <div className="col-span-1">
-                <h1 className='text-4xl font-extrabold text-[var(--Very-dark-blue)] mr-[7rem]'>{data.main_article.title}</h1>
+                <h1 className='text-6xl font-extrabold text-[var(--Very-dark-blue)]'>{data.main_article.title}</h1>
               </div>
               <div className="col-span-1 flex flex-col justify-between h-full">
-                <p className='text-[var(--Dark-grayish-blue)] text-sm mb-4'>{data.main_article.description}</p>
+                <p className='text-[var(--Dark-grayish-blue)] text-sm mb-4 leading-[1.5rem]'>{data.main_article.description}</p>
                 <a
                   href="#"
                   className="w-1/2 text-center bg-[var(--Soft-red)] font-bold text-black py-2 px-4 self-start"
@@ -37,16 +37,15 @@ function App() {
                   {data.main_article.link}
                 </a>
               </div>
-
             </div>
           </div>
-          <div className="bg-[var(--Very-dark-blue)] p-6">
-            <h2 className='text-[var(--Soft-orange)] font-extrabold text-2xl mb-4'>News</h2>
+          <div className="bg-[var(--Very-dark-blue)] p-6 mb-8">
+            <h2 className='text-[var(--Soft-orange)] font-extrabold text-4xl mb-4'>News</h2>
             <div className="flex flex-col justify-between">
               {data.new_articles.map((article, index) => (
                 <div className="mb-4" key={index}>
-                  <h3 className='text-[var(--Off-white)] font-bold mb-1'>{article.title}</h3>
-                  <p className='text-[var(--Grayish-blue)] text-sm'>{article.description}</p>
+                  <h3 className='text-lg text-[var(--Off-white)] font-bold mb-1'>{article.title}</h3>
+                  <p className='text-[var(--Grayish-blue)] text-sm leading-[1.5rem]'>{article.description}</p>
                   {index !== data.new_articles.length - 1 && (
                     <hr className="border-t border-[var(--Grayish-blue)] my-6" />
                   )}
@@ -56,9 +55,15 @@ function App() {
           </div>
           {/* Second row */}
           {data.featured_articles.map((article) => (
-            <div className="bg-gray-400 p-6 rounded-lg shadow-lg" key={article.id}>
-              <h3>{article.title}</h3>
+              <div className='grid grid-cols-3 gap-6' key={article.id}>
+              <div className="col-span-1">
+              <img src={article.image} alt={article.title} className="w-full h-auto mb-4" />
+              </div>
+              <div className="col-span-2 flex flex-col gap-2">
+              <p className='text-[var(--Soft-orange)] font-extrabold text-4xl'>{article.id}</p>
+              <h3 className='text-[var(--Very-dark-blue)] font-bold text-xl '>{article.title}</h3>
               <p>{article.description}</p>
+              </div>
             </div>
           ))}
         </div>
